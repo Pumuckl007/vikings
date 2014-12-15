e91496f9f16c988087216b5542a3d6079f99b5e9
pumuckl007 <pumuckl007>
2014-12-14 16:07:00 -0800

    first commit

---
+++ js/Accordion.js
@@ -0,0 +1,36 @@
+var robotics = {};
+robotics.donationObjects = [];
+robotics.donationObjects.push(document.getElementById("donation-one"));
+robotics.donationObjects.push(document.getElementById("donation-two"));
+robotics.donationObjects.push(document.getElementById("donation-three"));
+robotics.donationObjects.push(document.getElementById("donation-four"));
+robotics.donationObjects.push(document.getElementById("donation-five"));
+robotics.donationObjects.push(document.getElementById("donation-six"));
+robotics.donationObjects.push(document.getElementById("donation-seven"));
+for(var i = 0; i<7; i++){
+  var donationObject = robotics.donationObjects[i];
+  donationObject.addEventListener('mouseover', function(event){
+    var element = robotics.findObject(event.target, "donation-amount");
+    element.style.width = "38.3%";
+    var index = robotics.donationObjects.indexOf(element);
+    var otherElements = robotics.donationObjects.slice(0);
+    otherElements.splice(index,1);
+    for(var k = 0; k<otherElements.length; k++){
+      otherElements[k].style.width="10%";
+    }
+  });
+  donationObject.addEventListener('mouseout', function(event){
+    for(var k = 0; k<7; k++){
+      robotics.donationObjects[k].style.width="14%";
+    }
+  });
+  donationObject.onclick = function(){
+    window.location.href="https://www.foundationofljhs.com/robotics-team.html";
+  }
+}
+robotics.findObject = function(currentElement, className){
+  if(currentElement.className != className){
+    return robotics.findObject(currentElement.parentElement, className);
+  }
+  return currentElement;
+}
---
+++ js/theTeamGen.js
@@ -0,0 +1,42 @@
+var robotics = {};
+robotics.pictures = document.getElementById("pictures");
+robotics.onLoadPeople = function(){
+  var people = document.getElementById("people").contentWindow.document.body.childNodes[0].innerHTML;
+  var persons = people.split("\n");
+  var row;
+  for(var i = 0; i< persons.length; i++){
+    var person = persons[i];
+      if(person !="" && person.indexOf("//")<0){
+      //Name,Bio
+      var personInfo = person.split(",");
+      if(i%4===0){
+        row = robotics.pictures.appendChild(document.createElement("div"));
+        row.className="person-row";
+      }
+      var bio = personInfo[1].replace(new RegExp("(comma)", 'g'), ",")
+      var leader = personInfo[2].indexOf("leader")!=-1;
+      row.appendChild(robotics.createPictureFrame(personInfo[0],bio,leader));
+    }
+  }
+}
+robotics.createPictureFrame = function(Name, Bio, leader){
+  var frame = document.createElement("div");
+  frame.className="picture-frame";
+  var title = frame.appendChild(document.createElement("div"));
+  if(leader)
+    title.className="picture-title-leader";
+  else
+    title.className="picture-title";
+  title.id=Name.toLowerCase().replace(new RegExp(" ", 'g'),"");
+  var titleText = title.appendChild(document.createElement("div"));
+  titleText.className="picture-title-text";
+  titleText.innerHTML=Name;
+  var picture = frame.appendChild(document.createElement("div"));
+  picture.className="persons-picture";
+  var image = picture.appendChild(document.createElement("img"));
+  image.src="/people/" + Name.toLowerCase().replace(new RegExp(" ", 'g'),"");
+  var text = frame.appendChild(document.createElement("div"));
+  text.className = "frame-bio";
+  text.innerHTML=Bio;
+  return frame;
+}
